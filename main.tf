569ad602174dcbb987f26615c54e077739c29ba8
amarendrazero <akamar.sahu@gmail.com>
2023-09-03 11:37:43 +0530

    Update and rename main.tf.txt to main.tf
    ok version 1

---
+++ main.tf
@@ -0,0 +1,10 @@
+## Provider's Example
+
+# Configure the AWS Provider
+
+provider "aws" {
+   # version = "5.14.0"
+    access_key = "ACCESS_KEY"
+    secret_key = "SECRET_KEY"
+    region     = "us-east-1"
+}
