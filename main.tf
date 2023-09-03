dc30dfdd556a21c45be36815488cda326ed55ca4
amarendrazero <akamar.sahu@gmail.com>
2023-09-03 11:40:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,12 @@
 
 provider "aws" {
    # version = "5.14.0"
-    access_key = "ACCESS_KEY"
-    secret_key = "SECRET_KEY"
+    access_key = "AKIAYNQNQL525V7PUBPF"
+    secret_key = "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"
     region     = "us-east-1"
+}
+
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = "ami-0b0ea68c435eb488d"
+  instance_type = "t2.micro"
 }
